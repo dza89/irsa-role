b15c864c868857c7c2005d833278a09d88c5039b
dza89 <20373984+dza89@users.noreply.github.com>
2022-06-29 16:27:47 +0200

    Update datasources.tf

--- datasources.tf
+++ datasources.tf
@@ -1,3 +1,7 @@
 data "aws_iam_openid_connect_provider" "cluster" {
   url = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
 }
+
+data "aws_eks_cluster" "cluster" {
+  name = var.cluster_name
+}
